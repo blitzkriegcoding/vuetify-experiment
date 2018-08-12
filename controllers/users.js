0862a9341ad8b57887acca1676d090ae04092ad6
Yrvin Escorihuel <yher136@gmail.com>
2018-08-11 22:52:12 -0400

    Adding login page, validation and real authentication endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,12 @@
 const User = require('../models/User.js');
 
+const passportJWT = require('passport-jwt');
+const jwt = require('jsonwebtoken');
+
+const ExtractJwt = passportJWT.ExtractJwt;
+const jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+jwtOptions.secretOrKey = '7e4f867ff7e3af6c53ae70f415b2ab4dc0fec486';
 module.exports.controller = (app) => {
   app.post('/users/register', (req, res) => {
     const name = req.body.name;
@@ -12,8 +19,35 @@ module.exports.controller = (app) => {
       password,
     });
     User.createUser(newUser, (error, user) => {
-      if (error) { console.log(error); }
+      if (error) {
+        res.status(422).json({
+          message: 'Something went wrong. Please try again after some time!!',
+        });
+      }
       res.send({ user });
     })
-  })
+  });
+
+  app.post('/users/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.getUserByEmail(email, (err, user) => {
+      if (!user) {
+        res.status(404).json({
+          message: 'The user does not exists!'
+        });
+      } else {
+        User.comparePassword(password, user.password, (error, isMatch) => {
+          if (error) throw error;
+          if (isMatch) {
+            const payload = { id: user.id };
+            const token = jwt.sign(payload, jwtOptions.secretOrKey);
+            res.json({ message: 'ok', token });
+          } else {
+            res.status(401).json({ message: 'The password is incorrect' });
+          }
+        });
+      }
+    });
+  });
 }
